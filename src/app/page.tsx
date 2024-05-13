0585113d2b14823126dc6539a0f9f264bf1fc3ab
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2024-05-13 18:31:14 +0300

    clsx & dymamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { headers } from "next/headers";
 import StatusLabel, { Status } from "./components/StatusLabel";
 
 
 export default function Home() {
+  console.log(headers());
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-xl">Home page{new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
